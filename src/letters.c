22d083234771639fddd01fc5ecb13845e16e12c6
Zhengli Wang <zhengliw@crunchystudio.cc>
2024-10-15 22:28:08 +0800

    Add more letters
    The rest of the alphabet should be now included

--- src/letters.c
+++ src/letters.c
@@ -1,11 +1,11 @@
 #include "draw.h"
 const char CBANNER_LETTERS [][CBANNER_LETTER_HEIGHT][CBANNER_LETTER_WIDTH] = {
 	{
-		"??????",
-		"??????",
-		"??????",
-		"??????",
-		"??????",
+		"      ",
+		"      ",
+		"      ",
+		"      ",
+		"      ",
 	},
 	{
 		" ---- ",
@@ -85,15 +85,115 @@ const char CBANNER_LETTERS [][CBANNER_LETTER_HEIGHT][CBANNER_LETTER_WIDTH] = {
 		"-   - ",
 	},
 	{
-		" -    ",
-		" |    ",
-		" |    ",
-		" |    ",
+		"-     ",
+		"|     ",
+		"|     ",
+		"|     ",
 		"------",
 	},
+	{
+		"--  --",
+		"| \\/ |",
+		"|    |",
+		"|    |",
+		"-    -",
+	},
+	{
+		"--   -",
+		"| \\  |",
+		"|  \\ |",
+		"|    |",
+		"-   --",
+	},
+	{
+		" ---- ",
+		"|    |",
+		"|    |",
+		"|    |",
+		" ---- ",
+	},
+	{
+		"----- ",
+		"|    |",
+		"----- ",
+		"|     ",
+		"|     ",
+	},
+	{
+		" ---- ",
+		"|    |",
+		"|    |",
+		"|   \\|",
+		" ----\\",
+	},
+	{
+		"----- ",
+		"|    |",
+		"----- ",
+		"|   \\ ",
+		"|    \\",
+	},
+	{
+		" -----",
+		"|     ",
+		" ---- ",
+		"     |",
+		"----- ",
+	},
+	{
+		"------",
+		"   |  ",
+		"   |  ",
+		"   |  ",
+		"   -  ",
+	},
+	{
+		"-    -",
+		"|    |",
+		"|    |",
+		"|    |",
+		" ---- ",
+	},
+	{
+		"-    -",
+		"|    |",
+		"|    |",
+		" |  | ",
+		"  --  ",
+	},
+	{
+		"-    -",
+		"|    |",
+		"|    |",
+		"| /\\ |",
+		"--  --",
+	},
+	{
+		"-    -",
+		" \\  / ",
+		"  ><  ",
+		" /  \\ ",
+		"-    -",
+	},
+	{
+		"-    -",
+		" \\  / ",
+		"  \\/  ",
+		"  |   ",
+		"  |   ",
+	},
+	{
+		"------",
+		"   /  ",
+		"  /   ",
+		" /    ",
+		"------",
+	},
+
+
 };
 
-int _cb_letter_index_map (char letter) {
+inline int _cb_letter_index_map (char letter) {
 	switch (letter) {
 		case 'A': return 1;
 		case 'B': return 2;
@@ -107,8 +207,21 @@ int _cb_letter_index_map (char letter) {
 		case 'J': return 10;
 		case 'K': return 11;
 		case 'L': return 12;
+		case 'M': return 13;
+		case 'N': return 14;
+		case 'O': return 15;
+		case 'P': return 16;
+		case 'Q': return 17;
+		case 'R': return 18;
+		case 'S': return 19;
+		case 'T': return 20;
+		case 'U': return 21;
+		case 'V': return 22;
+		case 'W': return 23;
+		case 'X': return 24;
+		case 'Y': return 25;
+		case 'Z': return 26;
 		default: return 0;
 	}
 }
-
 
